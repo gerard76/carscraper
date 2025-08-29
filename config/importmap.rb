f22292e650dff268d68e59e7e6834e9a326a763a
Gerard <gerard@eet.nu>
2025-08-29 13:34:51 +0200

    Fix echarts

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,4 @@
 pin "application", preload: true
 
-# Pin ECharts (optional if you want CDN)
-# You can also install via npm/yarn if using jsbundling
 pin "echarts", to: "echarts.min.js"
-pin "echarts/theme/dark", to: "echarts/theme/dark.js"
+pin "echarts/theme/dark", to: "dark.js"
